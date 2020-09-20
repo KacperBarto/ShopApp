2ad54d9d69d07841983fd7217ebc364f1ec5fe47
Kapar-hub <kacper.barto727@gmail.com>
2020-09-20 14:25:07 +0200

    Basic routing - update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainpageComponent} from './mainpage/mainpage.component';
+import {ProductsComponent} from './products/products.component';
+import {CartComponent} from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main-page', component: MainpageComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 import { CoreModule} from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    ProductsComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
