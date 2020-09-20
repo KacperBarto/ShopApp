4f9bc5b44945cb8fee4f39974c9520659d9c9cc4
Kapar-hub <kacper.barto727@gmail.com>
2020-09-20 17:44:09 +0200

    Routing - update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {MainpageComponent} from './mainpage/mainpage.component';
 import {ProductsComponent} from './products/products.component';
 import {CartComponent} from './cart/cart.component';
-
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component'
 const routes: Routes = [
   { path: 'main-page', component: MainpageComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'cart', component: CartComponent},
+  { path: '', redirectTo: 'main-page', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent}
+
 ];
 
 @NgModule({
