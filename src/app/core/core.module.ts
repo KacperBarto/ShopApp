e3458653463095636707f3140f4f23bebaa15cba
Spo0kky <47164010+spo0kky@users.noreply.github.com>
2020-09-21 13:41:38 +0200

    Walka z wiatrakami t branch -D <local-branch># Changes to be committed:

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [NavbarComponent],
-  exports: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent, HeaderComponent],
+  exports: [NavbarComponent, HeaderComponent],
   imports: [CommonModule, RouterModule],
 })
 export class CoreModule { }
